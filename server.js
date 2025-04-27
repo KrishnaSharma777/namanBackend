dadf20d0612698fcdc3378f8cc19f15304e436c5
krishnaonly1loan <krishna@only1loan.com>
2025-04-27 21:30:26 +0530

    add

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS for all routes (allowing all origins)
-app.use(cors());
+// app.use(cors());
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "https://www.archaspect.com", // frontend domain
+    credentials: true,
+  })
+);
+
 
 // Setup multer for handling form-data
 const upload = multer({ storage: multer.memoryStorage() }); // In-memory storage (no files saved to disk)
